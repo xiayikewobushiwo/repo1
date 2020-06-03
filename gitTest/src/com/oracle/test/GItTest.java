2a22aa3981c0855f869f2feff7faa96edf4a136f
yimo_global <yimoglobal@oracle.com>
2020-06-03 11:04:51 +0800

    1.0版本提交

--- gitTest/src/com/oracle/test/GItTest.java
+++ gitTest/src/com/oracle/test/GItTest.java
@@ -4,4 +4,10 @@ public class GItTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
     }
+
+    public void show(){
+        for (int i=0;i<100;i++){
+            System.out.println(i);
+        }
+    }
 }
