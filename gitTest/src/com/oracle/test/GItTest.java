84dbcbf86c930644de72f5d5d7a62bc3c7e88205
yimo_global <yimoglobal@oracle.com>
2020-06-03 11:48:14 +0800

    修改1.0 by  yimo

--- gitTest/src/com/oracle/test/GItTest.java
+++ gitTest/src/com/oracle/test/GItTest.java
@@ -1,13 +1,17 @@
 package com.oracle.test;
 
 public class GItTest {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello Git!");
+        //yimo的修改
+
     }
 
     public void show(){
         for (int i=0;i<100;i++){
             System.out.println(i);
+            System.out.println("我爱你，中国！");
         }
     }
 }
